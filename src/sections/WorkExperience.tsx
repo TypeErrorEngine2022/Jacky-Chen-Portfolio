1ec3a4dd447a32e1e91d095a31165662d4df2db4
Jacky <jackychen924@gmail.com>
2025-04-18 01:49:48 +0800

    refactor: extract component

--- src/sections/WorkExperience.tsx
+++ src/sections/WorkExperience.tsx
@@ -1,11 +1,12 @@
 import { Card, Tag, Timeline } from "antd";
+import { PropsWithChildren } from "react";
 
-const AtgWorkingExperience = () => (
+const AtgPartTimeWorkingExperience = () => (
 	<div>
 		<h3 className="text-xl font-semibold">
 			Part-time Software Developer @ Applied Technology Group Limited
 		</h3>
-		<p className="text-gray-600">Jun 2023 - Present</p>
+		<p className="text-gray-600">Sep 2023 - Present</p>
 		<ol className="mt-2 text-gray-700 list-disc">
 			<li>Revamp over 20 Vue components to React with documented QA</li>
 			<li>Took the initiative to develop a swagger compare tool to facilitate API upgrades</li>
@@ -15,6 +16,30 @@ const AtgWorkingExperience = () => (
 			</li>
 		</ol>
 	</div>
+);
+
+const AtgInternWorkingExperience = () => (
+	<div>
+		<h3 className="text-xl font-semibold">Intern @ Applied Technology Group Limited</h3>
+		<p className="text-gray-600">Jun 2023 - Aug 2023</p>
+		<ol className="mt-2 text-gray-700 list-disc">
+			<li>
+				Developed a responsive and scalable periodontal chart with React in the summer internship
+			</li>
+			<li>Assisted the transition to REST API using NestJS and TypeORM</li>
+		</ol>
+	</div>
+);
+
+const WorkingExperienceCard = ({
+	children,
+	color,
+	isPresent,
+}: PropsWithChildren<{ color: string; isPresent?: boolean }>) => (
+	<Card className={`w-full border-l-4 border-${color}-500`}>
+		<Tag color={isPresent ? color : "default"}>{isPresent === true ? "Present" : "Past"}</Tag>
+		{children}
+	</Card>
 );
 
 const WorkExperience: React.FC = () => (
@@ -28,21 +53,26 @@ const WorkExperience: React.FC = () => (
 				mode="left"
 				items={[
 					{
-						children: <AtgWorkingExperience />,
+						children: <AtgPartTimeWorkingExperience />,
 						label: "Present",
 						color: "green",
+					},
+					{
+						children: <AtgInternWorkingExperience />,
+						label: "Jun 2023 - Aug 2023",
+						color: "gray",
 					},
 				]}
 			/>
 		</div>
 
 		<div className="md:hidden space-y-4">
-			<Card className="w-full border-l-4 border-green-500">
-				<Tag color="green" className="mb-2">
-					Present
-				</Tag>
-				<AtgWorkingExperience />
-			</Card>
+			<WorkingExperienceCard color="green" isPresent>
+				<AtgPartTimeWorkingExperience />
+			</WorkingExperienceCard>
+			<WorkingExperienceCard color="gray">
+				<AtgInternWorkingExperience />
+			</WorkingExperienceCard>
 		</div>
 	</section>
 );
