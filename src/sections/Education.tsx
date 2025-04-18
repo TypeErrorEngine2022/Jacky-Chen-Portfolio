e8af1278467b519e54b67e12368e07ec43adda19
Jacky <jackychen924@gmail.com>
2025-04-18 16:30:00 +0800

    feat: add awards

--- src/sections/Education.tsx
+++ src/sections/Education.tsx
@@ -9,9 +9,11 @@ const Education: React.FC = () => (
 
 		<Card className="w-full border-l-4 border-blue-500 mb-6">
 			<div>
-				<div className="flex justify-between items-center">
+				<div className="flex flex-col md:flex-row md:justify-between md:items-center">
 					<h3 className="text-xl font-semibold">Bachelor of Science in Computer Science</h3>
-					<h4 className="text-lg font-medium mt-2">The Chinese University of Hong Kong (CUHK)</h4>
+					<h4 className="text-lg font-medium mt-2 md:mt-0">
+						The Chinese University of Hong Kong (CUHK)
+					</h4>
 				</div>
 				<p className="text-gray-600">Aug 2021 – Jul 2025 (Expected)</p>
 				<p className="text-gray-700">CGPA: 3.7 / 4.00</p>
@@ -22,6 +24,18 @@ const Education: React.FC = () => (
 						Databases, Operating Systems, Computer Networking, Blockchain Development, Basic Banking
 						and Finance
 					</p>
+				</div>
+
+				<div className="mt-4">
+					<p className="font-medium text-gray-700 text-lg">Awards & Honors:</p>
+					<ul className="list-disc pl-5 mt-2 text-gray-700">
+						<li>College Head's List 2022/23, 2023/24 from the Shaw College, CUHK</li>
+						<li>
+							Dean's List (2021-2022, 2022-2023, 2023-2024) from the Faculty of Engineering, CUHK
+						</li>
+						<li>Department/Programme Scholarships 2022/23 from the Shaw College, CUHK</li>
+						<li>Li Po Chun Charitable Trust Fund Undergraduate Scholarship 2021-2022</li>
+					</ul>
 				</div>
 			</div>
 		</Card>
